523245469dbc55f09a4f8cfdf350081cd834370e
Eugene <myyujinn@gmail.com>
2018-01-29 21:52:45 +0200

    Initial commit

---
+++ src/main/java/com/gmail/myyujinn/Spring5RecipeAppDemoApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.myyujinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeAppDemoApplication.class, args);
+	}
+}
